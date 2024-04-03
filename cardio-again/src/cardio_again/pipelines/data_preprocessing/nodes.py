1f0eb03e4a257e0e0fc11f1f0d7b8e579045cbad
renato <renato.costa87@gmail.com>
2024-04-03 14:23:31 -0300

    Add functions to replace zero blood pressure patients

--- cardio-again/src/cardio_again/pipelines/data_preprocessing/nodes.py
+++ cardio-again/src/cardio_again/pipelines/data_preprocessing/nodes.py
@@ -22,3 +22,41 @@ def _imput_median(list: pd.Series, df: pd.DataFrame,  sex: pd.StringDtype, media
             pass
 
     return df
+
+def _calculate_median_resting_bp(df: pd.DataFrame, age_range) -> float:
+    """
+    Calculate the median resting blood pressure for a given age range.
+    
+    Parameters:
+        df (DataFrame): The DataFrame containing patient data.
+        age_range (tuple): A tuple representing the age range as (start_age, end_age).
+        
+    Returns:
+        float: The median resting blood pressure.
+    """
+    median = df.loc[(df['Age'] >= age_range[0]) & (df['Age'] <= age_range[1]) & (df['RestingBP'] != 0), 'RestingBP'].median()
+    return median
+
+def _replace_zero_resting_bp(df: pd.DataFrame, age_range: list, median: float) -> pd.DataFrame:
+    """
+    Replace zero 'RestingBP' values with the calculated median based on age range.
+    
+    Parameters:
+        df (DataFrame): The DataFrame containing patient data.
+        age_range (tuple): A tuple representing the age range as (start_age, end_age).
+        median (float): The median resting blood pressure for the given age range.
+    """
+    df.loc[(df['Age'] >= age_range[0]) & (df['Age'] <= age_range[1]) & (df['RestingBP'] == 0), 'RestingBP'] = median
+    return df
+
+def _process_age_intervals(df: pd.DataFrame, age_intervals) -> pd.DataFrame:
+    """
+    Orchestrate the process by iterating through age intervals and calling the necessary functions.
+    
+    Parameters:
+        df (DataFrame): The DataFrame containing patient data.
+        age_intervals (list): A list of tuples representing age intervals as (start_age, end_age).
+    """
+    for age_range in age_intervals:
+        median = _calculate_median_resting_bp(df, age_range)
+        _replace_zero_resting_bp(df, age_range, median)
